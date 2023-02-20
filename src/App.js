053d3997b7a40090e2c5ecd0332a2cd24ec5c791
Ashish Selvaraj <52637731+SaurontheMighty@users.noreply.github.com>
2023-02-20 14:13:33 -0500

    AshishSelvaraj.com 2021

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import About from "./About";
+import Aside from "./Aside";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Header /> */}
+    <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Made with HTML, CSS, JavaScript and React by Ashish.</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
